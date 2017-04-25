8b699ba2e8443e78925e0e34d8033d4cf16ecdbe
Patrice Grand <Patrice Grand>
2017-04-25 11:28:55 +0200

    version initiale

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,25 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent }   from './app.component';
 import { ConnexionComponent }   from './connexion/app.connexion.component';
-import { MedecinsComponent }  from './medecins/app.medecins.component';
-import { VisitesComponent }  from './visites/app.visites.component';
-import { NavbarComponent }  from './navbar/app.navbar.component';
 import {DataService} from './services/app.service.data';
 
-const appRoutes: Routes = [
-  { path: '', component: ConnexionComponent },
-  { path: 'medecins', component: MedecinsComponent },
-  { path: 'visites', component: VisitesComponent },
-  { path: 'accueil',  component: NavbarComponent  }
-];
-
-
 @NgModule({
-  imports:      [ BrowserModule,FormsModule, HttpModule,RouterModule.forRoot(appRoutes) ],
-  declarations: [ AppComponent,ConnexionComponent,MedecinsComponent, VisitesComponent,NavbarComponent, ],
+  imports:      [ BrowserModule,FormsModule, HttpModule],
+  declarations: [ AppComponent,ConnexionComponent],
   providers : [DataService],
   bootstrap:    [ AppComponent ]
 })
