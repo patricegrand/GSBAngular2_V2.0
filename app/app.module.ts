887977c54499420dee59952d678fd9b12cbaa790
Patrice Grand <Patrice Grand>
2017-04-25 17:09:00 +0200

    maj 25-04-2017

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,16 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
-import { HttpModule } from '@angular/http';
 import { AppComponent }   from './app.component';
 import { ConnexionComponent }   from './connexion/app.connexion.component';
-import {DataService} from './services/app.service.data';
 
 @NgModule({
-  imports:      [ BrowserModule,FormsModule, HttpModule],
+  imports:      [ BrowserModule,FormsModule],
   declarations: [ AppComponent,ConnexionComponent],
-  providers : [DataService],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
